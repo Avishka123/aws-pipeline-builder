f33701e1f83bd371baca42e60c025d35f7ae8bd2
Avishka Rathnayake <123456789ymail@gmail.com>
2024-06-13 08:54:25 +0530

    docker changes

--- src/main/java/com/example/aws/code/pipeline/Application.java
+++ src/main/java/com/example/aws/code/pipeline/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsPipelineBuilderApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsPipelineBuilderApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
